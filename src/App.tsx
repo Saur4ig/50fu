750c75d6f9cf31aeb176322e05e7fefdbe6c3527
saur4ig <saurons909@gmail.com>
2025-02-09 22:25:55 +0100

    moved router to main file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Practice from "./pages/Practice";
 
 const App: React.FC = () => {
   return (
-    <Router>
       <div className="min-h-screen bg-gray-50 flex flex-col">
         <Navbar />
         <main className="flex-grow">
@@ -16,8 +15,6 @@ const App: React.FC = () => {
           </Routes>
         </main>
       </div>
-    </Router>
   );
 };
-
 export default App;
